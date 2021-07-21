217baae992a99d961e3753a84b246b3447e77283
EddyTeo <teoeddy46.20@ichat.sp.edu.sg>
2021-07-21 09:57:00 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
+url = 'https://ades-test-backend.herokuapp.com/';
 app.use(function (req, res) {
     return res.sendStatus(200);
 });
 
-app.listen(8000, function () {
+app.listen(url, function () {
     console.log('Sample Hosting app listening on port 8000');
 });
